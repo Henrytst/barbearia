c5b91f0f431486bdf623e6fe637dab2aa50faaa0
alexmohnnad <devweb.alex@gmail.com>
2023-04-10 23:10:46 -0300

    menu lateral

--- pages/functions/js/functions.js
+++ pages/functions/js/functions.js
@@ -1,14 +1,66 @@
+
+function pad(valor) { // completa com zeros à esquerda, caso necessário
+  return valor.toString().padStart(2, '0');
+}
+
+function formata(data) {
+  return `${data.getFullYear()}-${pad(data.getMonth() + 1)}-${pad(data.getDate())}`;
+}
+
+const campo = document.querySelector('#horario');
+
+window.addEventListener('DOMContentLoaded', function () {
+  var data = new Date(); // data de hoje
+  campo.min = formata(data);
+  // 2 anos à frente
+  data.setFullYear(data.getFullYear() + 2);
+  campo.max = formata(data);
+});
+
+// mensagens de validação
+campo.addEventListener('input', () => {
+  campo.setCustomValidity('');
+  campo.checkValidity();
+});
+
+// se tentar submeter o form com data fora do intervalo, mostra o erro
+campo.addEventListener('invalid', () => {
+  campo.setCustomValidity('A data deve estar entre hoje e 2 anos à frente');
+});
+
+
+
+
 $(function () {
   jQuery.datetimepicker.setLocale('pt-BR');
   $('.horario').datetimepicker({
     format: 'd/m/Y H:i',
-    minDate:0,
-    defaultTime:'10:00',
     allowTimes: ['10:00', '10:30', '11:00', '11:30', '12:00',
       '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30'],
-      beforeShowDay: function (date) {
-        var day = date.getDay();
-        return [(day != 0 && day != 1)];
-      }
+    beforeShowDay: function (date) {
+      var day = date.getDay();
+      return [(day != 0 && day != 1)];
+    }
   });
 });
+//função do menu lateral 
+$(function menu() {
+  var menu_width = 290;
+  var menu = $(".menu");
+  var menu_open = $(".menu-open");
+  var menu_close = $(".menu-close");
+  var overlay = $(".overlay");
+ 
+  menu_open.click(function (e) {
+    e.preventDefault();
+    menu.css({"right": "0px"});
+    overlay.css({"opacity": "1", "width": "100%"});
+  });
+  
+  menu_close.click(function (e) {
+    e.preventDefault();
+    menu.css({"right": "-" + menu_width + "px"});
+    overlay.css({"opacity": "0", "width": "0"});
+  });
+ });
+//função bloquear horario
--- pages/functions/php/functions.php
+++ pages/functions/php/functions.php
@@ -8,22 +8,23 @@ function head()
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap5.min.css">
-    <link rel="stylesheet" href="/barbearia/pages/CSS/style.css">
+    <link rel="stylesheet" href="/pages/CSS/style.css">"
 <?php
 }
 
 function menu()
 {
     include('\Xampp\htdocs\barbearia\pages\protect.php');
-?>
-    <input type="checkbox" id="check">
-    <label for="check" id="labelMenu">
-        <img src="\barbearia\pages\IMAGES\menu.png" id="imgMenu" style="cursor:pointer;">
-    </label>
-    <nav id="menu">
+?>      <div class="container-menu">
+        <a href="#" class="menu-open">Menu</a>
+        </div>
+    <nav>
+        <div class="overlay"></div>
+        <div class="menu">
+        <a href="#" class="menu-close">&times;</a> 
         <ul id="ulMenu">
             <li>
-                <a href="\barbearia\pages\cadastro.php" class="linkMenu">Início</a>
+                <a href="cadastro.php" class="linkMenu">Início</a>
             </li>
             <li>
                 <a href="cadastro.php" class="linkMenu">Cadastro</a>
@@ -64,8 +65,9 @@ function rodape()
     </script>
     <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap5.min.js"></script>
-    <script type="text/javascript" src="/barbearia/pages/functions/js/jquerymask/dist/jquery.mask.min.js"></script>
-    <script type="text/javascript" src="/barbearia/pages/functions/js/mascaras.js"></script>
+    <script type="text/javascript" src="/pages/functions/js/jquerymask/dist/jquery.mask.min.js"></script>
+    <script type="text/javascript" src="/pages/functions/js/mascaras.js"></script>
+    <script src="/pages/functions/js/functions.js"></script>
 <?php
 }
 
@@ -78,11 +80,11 @@ function headFormulario()
     <title>Cadastro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="/barbearia/pages/CSS/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.2.3/flatpickr.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.2.3/themes/dark.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.4/jquery.datetimepicker.min.css">
-
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="/pages/CSS/style.css">
 <?php
 }
 
@@ -90,13 +92,14 @@ function rodapeFormulario()
 {
 ?>
     <!-- Adicionando JQuery -->
-    <script src="\barbearia\pages\functions\js\jquery.js"></script>
+    <script src="/pages/functions/js/jquery.js"></script>
     <!-- Adicionando Javascript -->
-    <script type="text/javascript" src="/barbearia/pages/functions/js/jquerymask/dist/jquery.mask.min.js"></script>
-    <script type="text/javascript" src="/barbearia/pages/functions/js/mascaras.js"></script>
-    <script type="text/javascript" src="\barbearia\pages\functions\js\datatimepicker.js"></script>
+    <script type="text/javascript" src="/pages/functions/js/jquerymask/dist/jquery.mask.min.js"></script>
+    <script type="text/javascript" src="/pages/functions/js/mascaras.js"></script>
+    <script type="text/javascript" src="/pages/functions/js/datatimepicker.js"></script>
     <!-- Adicionando Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="/pages/functions/js/mascaras.js"></script>
 <?php
 }
