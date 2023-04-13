3b760a70350eedbd1f87c382192a673b142d77e7
Henrytst <testihenrique@gmail.com>
2023-04-12 21:57:20 -0300

    1.1.1

--- pages/functions/js/functions.js
+++ pages/functions/js/functions.js
@@ -35,12 +35,20 @@ $(function () {
   jQuery.datetimepicker.setLocale('pt-BR');
   $('.horario').datetimepicker({
     format: 'd/m/Y H:i',
+    minDate:0,
+    defaultTime:'10:00',
     allowTimes: ['10:00', '10:30', '11:00', '11:30', '12:00',
       '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30'],
     beforeShowDay: function (date) {
       var day = date.getDay();
       return [(day != 0 && day != 1)];
     }
+    /*Quando o barbeiro quiser pausar em certas datas*/
+    /*disabledDates: [
+      moment("2023/04/21"),
+      new Date(2023, 04, 21),
+      "2023/04/30 00:53"
+    ],*/
   });
 });
 //função do menu lateral
